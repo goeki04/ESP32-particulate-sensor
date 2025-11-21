16c42203a8fedf24e6ed7a0c2d3fd993838fbc7a
Goekdeniz Koeksal <flection.gd@gmail.com>
2025-11-21 16:18:28 +0100

    Extended the subsystem interface and the corresponding classes

--- src/GuiManager.h
+++ src/GuiManager.h
@@ -6,6 +6,7 @@ class GuiManager : public ISubsystem{
     public:
     void start() override;
     void update() override;
+    void updateEvent(SDL_Event* event) override;
     void destroy() override;
     private:
     WindowManager* m_WindowManager = SystemManager::getInstance().getSubsystem<WindowManager>();
