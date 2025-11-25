fb0d9bd09505137d3c7e4e35192afdb15bbd979e
Goekdeniz Koeksal <flection.gd@gmail.com>
2025-11-25 09:41:19 +0100

    Added menu bar and notification widget

--- src/GuiManager.h
+++ src/GuiManager.h
@@ -1,10 +1,15 @@
 #pragma once
 class GuiManager{
     public:
-    bool showDemoWindow = true;
-    bool showAnotherWindow = false;
-    ImVec4 clearColor = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     void init(SDL_Window* window, SDL_Renderer* renderer);
     void draw();
     void destroy();
+private:
+    void drawNavBar();
+    void drawNotifications();
+    void drawInformation();
+    int m_WindowWidth;
+    int m_WindowHeight;
+    float m_MenuBarHeight;
+    ImGuiWindowFlags m_WindowFlags = 0;
 };
