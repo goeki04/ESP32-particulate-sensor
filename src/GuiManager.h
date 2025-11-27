8eb079df99e92d717a157464731c278c83e87f90
Koeksal Goekdeniz (WaP/TGA) <kog1wa1@bosch.com>
2025-11-27 15:24:15 +0100

    Create framebuffer for viewport

--- src/GuiManager.h
+++ src/GuiManager.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "glm/glm.hpp"
 struct Margin {
     float left;
     float right;
@@ -31,6 +32,9 @@ class GuiManager{
     public:
     void init(SDL_Window* window);
     void update();
+    float getMenuBarHeight();
+    ImVec2 getNewWindowPos(Margin margin, ImVec2 windowSize, Alignment alignment);
+    ImVec4 getViewportPosScale();
 private:
     void drawNavBar();
     void drawNotification();
@@ -39,11 +43,30 @@ private:
     void drawMeasurementDisplay();
     void setFlags();
     void setStyle();
-    ImVec2 getNewWindowPos(Margin margin, ImVec2 windowPos, ImVec2 windowSize, Alignment alignment);
     int m_WindowWidth = 0;
     int m_WindowHeight = 0;
+    glm::vec4 m_ViewportPosScale;
     float m_MenuBarHeight = 0;
     float m_WidgetWidth = 0;
     float m_MarginDefault = 0;
     ImGuiWindowFlags m_WindowFlags = 0;
+    //debug
+    bool m_Debug = false;
 };
+
+inline bool operator==(const ImVec2& a, const ImVec2& b) {
+    return a.x == b.x && a.y == b.y;
+}
+/// <summary>
+/// Used for debugging with glm
+/// </summary>
+/// <returns>os</returns>
+inline std::ostream& operator<<(std::ostream& os,const glm::vec4& a) {
+    os << "(" << a.x << ", " << a.y << ", " << a.z << ", " << a.w << ")";
+    return os;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const ImVec2& a) {
+    os << "(" << a.x << ", " << a.y << ")";
+    return os;
+}
