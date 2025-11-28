d643225e76e539907cb5500340177a46b2da9fa5
Koeksal Goekdeniz (WaP/TGA) <kog1wa1@bosch.com>
2025-11-28 11:11:22 +0100

    added assimp as dependency

--- conanfile.py
+++ conanfile.py
@@ -7,8 +7,9 @@ class CmakeTest(ConanFile):
     def requirements(self):
         self.requires("glm/1.0.1")
         self.requires("sdl/3.2.20")
-        self.requires("stb/cci.20240531")
         self.requires("imgui/1.92.4")
         self.requires("glew/2.2.0")
+        self.requires("assimp/6.0.2")
+        self.requires("stb/cci.20230920")
     def layout(self):
         cmake_layout(self)
