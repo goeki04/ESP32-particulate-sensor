285245886efb26925aa08583876dce048431fc5d
Goekdeniz Koeksal <flection.gd@gmail.com>
2025-12-01 15:38:06 +0100

    Vertex and index buffer are done! Shader compilation groundwork done!

---
+++ src/shader/vertexShader.glsl
@@ -0,0 +1,9 @@
+#version 460 core
+layout(location = 0) in vec3 aPos;   // Vertex-Position
+uniform mat4 model;
+uniform mat4 projection;
+uniform mat4 view;
+void main()
+{
+    gl_Position = projection * view * model * vec4(aPos,1.0);
+}
