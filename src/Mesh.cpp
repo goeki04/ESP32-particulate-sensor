7bdf47b02d72107a4e0c8de6323bca7ee126d87a
Goekdeniz Koeksal <flection.gd@gmail.com>
2026-01-05 15:34:56 +0100

    Added color buffer

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -26,6 +26,8 @@ void Mesh::createMesh()
     //texCoords
     glEnableVertexAttribArray(2);
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, uv));
+    glEnableVertexAttribArray(3);
+    glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color));
     glBindVertexArray(0);
 }
 
