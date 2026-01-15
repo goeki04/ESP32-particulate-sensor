0df93151e53cfd2dbdba98877c56e586f2a04fd3
Goekdeniz Koeksal <flection.gd@gmail.com>
2026-01-15 13:53:21 +0100

    added grid axis color

--- src/shader/gridFragment.glsl
+++ src/shader/gridFragment.glsl
@@ -9,8 +9,19 @@ uniform vec4 gGridColorThick = vec4(0.0,0.0,0.0,1.0);
 uniform vec3 camPos;
 uniform float gGridSize = 100.0;
 
-float satf(float x) { return clamp(x, 0.0, 1.0); }
-float log10f(float x) { return log(x) / log(10.0); }
+vec3 xAxisColor = vec3(1.0,0.0,0.0);
+vec3 yAxisColor = vec3(0.0,1.0,0.0);
+vec3 zAxisColor = vec3(0.0,0.0,1.0);
+
+float satf(float x) 
+{ 
+    return clamp(x, 0.0, 1.0); 
+}
+float log10f(float x) 
+{ 
+    return log(max(x, 1e-6)) / log(10.0);
+}
+
 void main(){
     vec2 dvx = vec2(dFdx(worldPos.x), dFdy(worldPos.x));
     vec2 dvy = vec2(dFdx(worldPos.z), dFdy(worldPos.z));
@@ -24,6 +35,7 @@ void main(){
     float gridCellSizeLOD0 = gGridCellSize * pow(10.0,floor(LOD));
     float gridCellSizeLOD1 = gridCellSizeLOD0 * 10.0;
     float gridCellSizeLOD2 = gridCellSizeLOD1 * 10.0;
+
     dudv = max(dudv * 4.0, vec2(1e-6));
 
     vec2 mod_div_dudv = mod(worldPos.xz, gridCellSizeLOD0) / dudv;
@@ -39,6 +51,10 @@ void main(){
     float Lod2a = max(c.x, c.y);
 
     float LOD_fade = fract(LOD);
+
+    float xLine = 1.0 - satf(abs(worldPos.x) / dudv.x);
+    float zLine = 1.0 - satf(abs(worldPos.z) / dudv.y);
+
     vec4 color;
     if(Lod2a > 0.0){
         color = gGridColorThick;
@@ -46,17 +62,26 @@ void main(){
     }
     else{
         if(Lod1a > 0.0){
-            color = mix(gGridColorThick, gGridColorThin,LOD_fade);
+            color = mix(gGridColorThick, gGridColorThin, LOD_fade);
             color.a *= Lod1a;
         }
         else{
             color = gGridColorThin;
-            color.a *= (Lod0a * (1.0- LOD_fade));
+            color.a *= (Lod0a * (1.0 - LOD_fade));
         }
+    }
+
+    if(xLine > 0.0) {
+        color.rgb = mix(color.rgb, zAxisColor, xLine);
+        color.a = max(color.a, xLine);
+    }
+    if(zLine > 0.0) {
+        color.rgb = mix(color.rgb, xAxisColor, zLine);
+        color.a = max(color.a, zLine);
     }
 
     float opacityFallof = (1.0 - satf(length(worldPos.xz - camPos.xz) / gGridSize));
     color.a *= opacityFallof;
-    
+
     FragColor = color;
 }
