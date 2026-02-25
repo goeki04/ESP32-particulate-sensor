f8f49f373d9e84ba391bff99d089fa8caac3cc70
Goekdeniz Koeksal <flection.gd@gmail.com>
2026-02-25 14:04:56 +0100

    added EspHomeClient class

---
+++ src/ESPHomeClient.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+#include "EspHomeDecoder.h"
+class ESPHomeClient {
+public:
+	ESPHomeClient();
+	~ESPHomeClient();
+
+	void connect(const std::string& host, const std::string& port);
+
+	ESPHomeDecoder& getDecoder() { return m_Decoder; }
+private:
+	void handleResolve(asio::ip::tcp::resolver::results_type results);
+	void handleConnect(const asio::ip::tcp::endpoint& endpoint);
+	void startRead();
+	asio::io_context m_IoContext;
+	asio::ip::tcp::resolver m_Resolver;
+	asio::ip::tcp::socket m_Socket;
+	uint8_t m_ReceiveBuffer[1024];
+	std::thread m_NetworkThread;
+	ESPHomeDecoder m_Decoder;
+};
