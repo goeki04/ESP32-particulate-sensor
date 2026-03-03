4d29f40fe1ff6f0caeb1b6dc46ee5a2b0044dcaf
Goekdeniz Koeksal <flection.gd@gmail.com>
2026-03-03 12:28:33 +0100

    Added ninja to toolchain

--- build.ps1
+++ build.ps1
@@ -1,26 +1,2 @@
-﻿if (-not (Test-Path "conan")) {
-    New-Item -ItemType Directory -Path "conan" | Out-Null
-}
-$toolchainFile = Join-Path $PSScriptRoot "conan/build/generators/conan_toolchain.cmake"
-if (-not (Test-Path "cmake")) {
-    New-Item -ItemType Directory -Path "cmake" | Out-Null
-}
-
-conan install . `
-    -s compiler.cppstd=20 `
-    -s build_type=Debug `
-    -of conan/ `
-    --build=missing
-
-conan install . `
-    -s compiler.cppstd=20 `
-    -s build_type=Release `
-    -of conan/ `
-    --build=missing
-
-Set-Location "cmake"
-cmake .. `
-    -DCMAKE_BUILD_TYPE=Debug `
-    -"DCMAKE_TOOLCHAIN_FILE=$toolchainFile"
-cmake --build . --config Debug
-Read-Host "Press enter to close."
+﻿conan install . -s compiler.cppstd=20 -s build_type=Debug -of conan/ --build=missing -c tools.cmake.cmaketoolchain:generator=Ninja
+conan install . -s compiler.cppstd=20 -s build_type=Release -of conan/ --build=missing -c tools.cmake.cmaketoolchain:generator=Ninja
