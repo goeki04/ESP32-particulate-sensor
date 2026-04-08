b85dd7991d47075645b05c841de356df65dc7701
Goekdeniz Koeksal <flection.gd@gmail.com>
2026-04-08 11:59:07 +0200

    fixed python dependency

---
+++ modules/definitions/generated_component_names.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string_view>
+#include "a_components.hpp"
+
+namespace Andromeda::ECS::Component {
+
+    template<typename T>
+    constexpr std::string_view get_component_name() { return "Unknown"; }
+    template<> constexpr std::string_view get_component_name<AABB>() { return "AABB"; }
+    template<> constexpr std::string_view get_component_name<Mesh>() { return "Mesh"; }
+    template<> constexpr std::string_view get_component_name<Selected>() { return "Selected"; }
+    template<> constexpr std::string_view get_component_name<Tag>() { return "Tag"; }
+    template<> constexpr std::string_view get_component_name<Transform>() { return "Transform"; }
+
+}
