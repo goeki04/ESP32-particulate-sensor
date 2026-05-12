e6c5c29d9611c848811274dad241440a6606b2c2
Goekdeniz Koeksal <flection.gd@gmail.com>
2026-05-12 10:32:19 +0200

    working undo for transform component in editor and viewport

--- modules/gui/Widgets/component_ui.hpp
+++ modules/gui/Widgets/component_ui.hpp
@@ -3,14 +3,16 @@
 #include "a_registry.hpp"
 #include "imgui.h"
 #include "generated_components.hpp"
+#include <any>
 #include "resource_manager.h"
+#include "a_event_manager.hpp"
 namespace Andromeda::Gui::Component
 {
     template<typename T>
-    inline void drawComponentUI(ECS::EntityHandle handle){
+    inline void drawComponentUI(ECS::EntityHandle handle, std::any& undoState){
     }
     template<>
-    inline void drawComponentUI<ECS::Component::Transform>(ECS::EntityHandle handle)
+    inline void drawComponentUI<ECS::Component::Transform>(ECS::EntityHandle handle, std::any& undoState)
     {
             auto& transform = handle.get<ECS::Component::Transform>();
             auto& position = transform.position;
@@ -20,6 +22,14 @@ namespace Andromeda::Gui::Component
             ImGui::SameLine(100);
             ImGui::DragFloat3("##pos", &position.x, 0.1f);
 
+            if (ImGui::IsItemActivated()) {
+                undoState = transform;
+            }
+
+            if (ImGui::IsItemDeactivatedAfterEdit()) {
+                EventManager::getInstance().Dispatch(EventType::OnPushUndoTransform,
+                    PushUndoTransformEvent(handle.id, undoState));
+            }
             ImGui::Text("Rotation");
             ImGui::SameLine(100);
             vec3 rotationRad = amath::eulerAngles(transform.rotation);
@@ -28,21 +38,31 @@ namespace Andromeda::Gui::Component
                 transform.rotation = quat(amath::radians(rotationDeg));
             }
 
+            if (ImGui::IsItemActivated()) undoState = transform;
+            if (ImGui::IsItemDeactivatedAfterEdit()) {
+                EventManager::getInstance().Dispatch(EventType::OnPushUndoTransform,
+                    PushUndoTransformEvent(handle.id, undoState));
+            }
+
             ImGui::Text("Scale");
             ImGui::SameLine(100);
             ImGui::DragFloat3("##scale", &scale.x, 0.1f);
-
+            if (ImGui::IsItemActivated()) undoState = transform;
+            if (ImGui::IsItemDeactivatedAfterEdit()) {
+                EventManager::getInstance().Dispatch(EventType::OnPushUndoTransform,
+                    PushUndoTransformEvent(handle.id, undoState));
+            }
             ImGui::PopID();
             ImGui::Spacing();
     }
 
-    inline void renderEntityComponentUI(const ECS::EntityHandle handle, ResourceManager* rm)
+    inline void renderEntityComponentUI(const ECS::EntityHandle handle, ResourceManager* rm, std::any& undoState)
     {
         std::apply([&]<typename... T>(T&&... args)
         {
             if (ImGui::CollapsingHeader("Transform", ImGuiTreeNodeFlags_DefaultOpen))
             {
-                (drawComponentUI<T>(handle), ...);
+                (drawComponentUI<T>(handle,undoState), ...);
             }
         },ECS::Component::ComponentDirectory{});
     }
