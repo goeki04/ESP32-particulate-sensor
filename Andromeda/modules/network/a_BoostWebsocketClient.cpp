b7bd219bedf29352e9416c424ab9d72677872d45
Goekdeniz Koeksal <flection.gd@gmail.com>
2026-07-01 15:47:17 +0200

    rewriting websocket code now with multithreading

---
+++ Andromeda/modules/network/a_BoostWebsocketClient.cpp
@@ -0,0 +1,18 @@
+#include "a_BoostWebsocketClient.hpp"
+
+namespace Andromeda
+{
+	void BoostWebsocketClient::connect() {
+
+	}
+	void BoostWebsocketClient::disconnect() {
+
+	}
+	void BoostWebsocketClient::send(const std::string& payload) {
+
+	}
+
+	void BoostWebsocketClient::onMessageReceived(std::function<void(const std::string&)> callback) {
+	
+	}
+}
