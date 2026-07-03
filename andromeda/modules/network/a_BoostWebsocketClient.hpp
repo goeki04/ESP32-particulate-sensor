e0cbf1e2f48237fc2d8d02f31e86e11acb12b451
Koeksal Goekdeniz (WaP/TGA) <kog1wa1@bosch.com>
2026-07-03 15:18:31 +0200

    added network thread ans websocket implementation finished

--- andromeda/modules/network/a_BoostWebsocketClient.hpp
+++ andromeda/modules/network/a_BoostWebsocketClient.hpp
@@ -5,8 +5,11 @@
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ssl/context.hpp>
+#include <atomic>
 #include <boost/beast/ssl.hpp>
 #include <boost/asio/strand.hpp>
+#include <boost/asio/awaitable.hpp>
+#include <memory>
 #include <string>
 #include "a_network_info.hpp"
 #include "a_IWebsocketClient.hpp"
@@ -18,20 +21,28 @@ using tcp = boost::asio::ip::tcp;
 
 namespace Andromeda {
 
-	class BoostWebsocketClient : public IWebsocketClient {
+	class BoostWebsocketClient : public IWebsocketClient, public std::enable_shared_from_this<BoostWebsocketClient> {
 	public:
-		BoostWebsocketClient(boost::asio::io_context& io_context, boost::asio::ssl::context& ssl_context, WebsocketReq req,const std::optional<ProxySettings>& proxy_settings)
-			: m_IoContext(io_context), m_SslContext(ssl_context), m_ProxySettings(proxy_settings), m_Req(req), m_Stream(net::make_strand(m_IoContext), m_SslContext) {}
+		BoostWebsocketClient(std::shared_ptr<boost::asio::io_context> io_context, boost::asio::ssl::context& ssl_context, WebsocketReq req,const std::optional<ProxySettings>& proxy_settings)
+			: m_IoContext(io_context), m_SslContext(ssl_context), m_ProxySettings(proxy_settings), m_Req(req), m_Stream(net::make_strand(*m_IoContext), m_SslContext) {}
 		void connect() override;
 		void disconnect() override;
 		void send(const std::string& payload) override;
-
+		bool isConnected() const override { return m_IsConnected.load(std::memory_order_acquire); }
 		void onMessageReceived(std::function<void(const std::string&)> callback) override;
 	private:
-		boost::asio::io_context& m_IoContext;
+		std::shared_ptr<boost::asio::io_context> m_IoContext;
 		boost::asio::ssl::context& m_SslContext;
+		std::atomic<bool> m_IsConnected{ false };
 		std::optional<ProxySettings> m_ProxySettings;
 		WebsocketReq m_Req;
 		websocket::stream<beast::ssl_stream<beast::tcp_stream>> m_Stream;
+
+		std::function<void(const std::string&)> m_MessageCallback;
+		void startReadLoop();
+		static net::awaitable<void> doConnect(std::shared_ptr<BoostWebsocketClient> self);
+		static net::awaitable<void> doDisconnect(std::shared_ptr<BoostWebsocketClient> self);
+		static net::awaitable<void> doSend(std::shared_ptr<BoostWebsocketClient> self, std::string payload);
+		static net::awaitable<void> doReadLoop(std::shared_ptr<BoostWebsocketClient> self);
 	};
 }
--- andromeda/modules/network/a_HomeAssistant.hpp
+++ andromeda/modules/network/a_HomeAssistant.hpp
@@ -1,11 +1,28 @@
 #pragma once
 #include "a_Primitives.hpp"
-
+#include "a_IWebsocketClient.hpp"
 #include <string>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ssl/context.hpp>
+#include "a_network_info.hpp"
+#include <optional>
+#include <memory>
 namespace Andromeda {
 	class HomeAssistantService {
 	public:
+		HomeAssistantService(std::shared_ptr<boost::asio::io_context> ioContext,
+			boost::asio::ssl::context& sslContext,
+			std::optional<ProxySettings> proxySettings)
+			: m_IoContext(std::move(ioContext)), m_SslContext(sslContext), m_ProxySettings(std::move(proxySettings)) {}
+
 		void init();
 		std::string readHomeAssistantTokenFromSecretsJson();
+	private:
+		std::shared_ptr<boost::asio::io_context> m_IoContext;
+		boost::asio::ssl::context& m_SslContext;
+		std::optional<ProxySettings> m_ProxySettings;
+		i32 m_MessageID = 1;
+		bool m_Authenticated = false;
+		std::shared_ptr<IWebsocketClient> m_WebsocketClient;
 	};
 }
